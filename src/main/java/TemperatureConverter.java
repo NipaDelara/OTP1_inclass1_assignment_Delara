d4f6a8a92a1492d6ab6f96f60852599c32415940
NipaDelara <nipadelara@hotmail.com>
2026-09-03 17:12:51 +0300

    Implement TemperatureConverter class fahrenheitToCelsius()

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,2 +1,15 @@
 public class TemperatureConverter {
+    public double fahrenheitToCelsius(double fahrenheit){
+        return (fahrenheit - 32) * 5/9;
+    }
+    public double celsiusToFahrenheit(double celsius){
+        return (celsius * 5/9 )+ 32 ;
+    }
+    public boolean isExtremeTemperature(double celsius){
+        if(celsius < -40 || celsius > 50 ){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
